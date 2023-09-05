36a997462c64893a6800958d48a7ea39bf856e74
somanath <somanathg121@gmail.com>
2023-09-05 23:52:50 +0530

    first commit

---
+++ src/app/header.componenr.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//create simple ts class and make it component class @component
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html'
+//    template:'<h3>Ecom</h3>'
+    //if template html is small then write here but it is big then use templateurl.
+})
+export class HeaderComponent{
+
+}
